cf1df409e8d8ce863ff89071a1a0dda0d005fa83
nobel7761 <habiburnobel@gmail.com>
2022-05-05 03:58:59 +0600

    mongodb is connected to the server side

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,3 +16,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Server Running...");
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('electronics-warehouse').collection('products');
+
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
