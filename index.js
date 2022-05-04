b080a71173f8761dd82254c5b20f54888642c21c
nobel7761 <habiburnobel@gmail.com>
2022-05-05 03:25:10 +0600

    project folder is ready

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Warehouse");
+})
+
+app.listen(port, () => {
+    console.log("Server Running...");
+})
