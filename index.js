32d5e50af493b37810b58b395002d6d5d7160ce6
nobel7761 <habiburnobel@gmail.com>
2022-05-08 04:58:02 +0600

    product delete api modified

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ async function run() {
             res.send(result);
         })
 
+        //product delete api
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
