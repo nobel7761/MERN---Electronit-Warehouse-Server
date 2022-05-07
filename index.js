799741b52a65aaf14b144252fe013415f1dcec79
nobel7761 <habiburnobel@gmail.com>
2022-05-08 04:52:48 +0600

    api modified

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.listen(port, () => {
 })
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
