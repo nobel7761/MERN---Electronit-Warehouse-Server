a257f5f73a09e5eb3e1b7de18b595cf22513437c
nobel7761 <habiburnobel@gmail.com>
2022-05-08 04:56:42 +0600

    modified product stock and sold using delivered button api modified

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
             res.send(products)
         })
 
-        //delivered product
+        //updating product quantity and sold using delivered button
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const updateInfo = req.body;
